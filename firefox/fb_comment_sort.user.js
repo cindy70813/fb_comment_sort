fcdd5b74a68075615a0faeff6b0b21ea10eab762
mscarchilli <mikenytola@gmail.com>
2013-02-20 12:32:28 -0800

    1. Fixed bug that was causing script to not work.

--- firefox/fb_comment_sort.user.js
+++ firefox/fb_comment_sort.user.js
@@ -2,7 +2,7 @@
 // @name			Facebook Comment Chronological Sort
 // @author			Michael J Scarchilli & Bunnies... lots and lots of bunnies!
 // @namespace                   http://www.mikevision.com
-// @version			1.3
+// @version			1.4
 // @description                 Sorts individual post comments in chronological order on a Facebook "Page".
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @include			https://www.facebook.com/*
@@ -30,78 +30,70 @@ function main() {
     // FOR JQUERY SCRIPTS //
     (function($){
 	
-	$(document).ready(function(){	    
-	    if ($('body').hasClass('pagesTimelineLayout')){
-		addSortButton();     	     
-    
-		// ADD "SORT" BUTTON WHEN fbTimelineUnit INSERTED INTO DOM //
-		$(document).bind('DOMNodeInserted', function(e) {
-		    if($(e.target).hasClass('lastCapsule')){
-			addSortButton();
-		    }
-		});	    
+	$(document).ready(function(){	  
+	    if($('body').hasClass('pagesTimelineLayout')){
+		addSortButton();     	         
+	    }
 	    
-		// "ADD SORT BUTTON" //
-		/*
-	    $('.pagesTimelineButtonPagelet').append('<span id="addSortButton" class="uiButtonGroup uiButtonGroupOverlay" style="margin-left:10px;"><span class="firstItem lastItem uiButtonGroupItem buttonItem"><span  class="uiButton uiButtonConfirm"><span class="uiButtonText">Add Sort Button</span></span></span></span>');
-	    
-	    $('#addSortButton').bind('click', function(){
-		addSortButton();
-	    });
-	    */
-	   
-		// "SORT BUTTON" //
-		function addSortButton(){	 
-		    $('.sortButton').remove();
+	    // "SORT BUTTON" //
+	    function addSortButton(){	 
+		$('.sortButton').remove();
 		
-		    // ADD SORT BUTTON //	
-		    $('.UIActionLinks').append('<span class="sortButton"> Â· <a title="Sort Comments" href="#"><span id="">Sort</span></a></span>');
+		// ADD SORT BUTTON //	
+		$('.UIActionLinks').append('<span class="sortButton"> · <a title="Sort Comments" href="#"><span id="">Sort</span></a></span>');
 	    
-		    $('.sortButton').bind('click', function(e){
-			e.preventDefault();
-			var postID = $(this).parents('div.fbTimelineFeedbackHeader').next('div').find('ul').attr('id');
-			var post = document.getElementById(postID);
-			sortList(post);
+		$('.sortButton').bind('click', function(e){
+		    e.preventDefault();
+		    var postID = $(this).parents('div.fbTimelineFeedbackHeader').next('div').find('ul').attr('id');
+		    var post = document.getElementById(postID);
+		    sortList(post);
+		});
+	    
+		function sortList(post){
+		    var commentArray = [];
+		    var commentID;
+		    var commentHTML;		
+		    var removePIDend;
+		    var PID;		
+		    var likeSentence = $(post).children('li.UFILikeSentence').remove();		
+		    var pagerRow = $(post).children('li.UFIPagerRow').remove();		
+		    var userComment = $(post).children('li.UFIAddComment').remove();		
+		
+		    // GET LI ID'S // 
+		    $(post).children('li, ul').each(function(){
+			commentID = (this.id);
+			commentHTML = (this.outerHTML);
+		    
+			removePIDend = commentID.substring(0, commentID.indexOf("}"));
+			PID = removePIDend.substr(removePIDend.lastIndexOf('_')+1);
+		    
+			commentArray.push({
+			    id:PID, 
+			    html:commentHTML
+			});   
 		    });
-	    
-		    function sortList(post){
-			var commentArray = [];
-			var commentID;
-			var commentHTML;		
-			var removePIDend;
-			var PID;		
-			var likeSentence = $(post).children('li.UFILikeSentence').remove();		
-			var pagerRow = $(post).children('li.UFIPagerRow').remove();		
-			var userComment = $(post).children('li.UFIAddComment').remove();		
 		
-			// GET LI ID'S // 
-			$(post).children('li, ul').each(function(){
-			    commentID = (this.id);
-			    commentHTML = (this.outerHTML);
-		    
-			    removePIDend = commentID.substring(0, commentID.indexOf("}"));
-			    PID = removePIDend.substr(removePIDend.lastIndexOf('_')+1);
-		    
-			    commentArray.push({
-				id:PID, 
-				html:commentHTML
-			    });   
-			});
+		    commentArray.sort(function(a,b){
+			return a.id-b.id;
+		    });
 		
-			commentArray.sort(function(a,b){
-			    return a.id-b.id;
-			});
-		
-			$(post).children('li,ul').remove();
-			for( var i =  0 ; i < commentArray.length ; ++i){
-			    $(post).append(commentArray[i].html);
-			}
-			$(likeSentence).prependTo(post);
-			$(pagerRow).appendTo(post);
-			$(userComment).appendTo(post);
+		    $(post).children('li,ul').remove();
+		    for( var i =  0 ; i < commentArray.length ; ++i){
+			$(post).append(commentArray[i].html);
 		    }
+		    $(likeSentence).prependTo(post);
+		    $(pagerRow).appendTo(post);
+		    $(userComment).appendTo(post);
 		}
 	    }
+		
+	    // ADD "SORT" BUTTON WHEN fbTimelineUnit INSERTED INTO DOM //
+	    $(document).bind('DOMNodeInserted', function(e) {
+		if($(e.target).hasClass('lastCapsule')){
+		    addSortButton();
+		}
+	    });
+		
 	// DOCUMENT READY END //
 	});
     })(jQuery);
